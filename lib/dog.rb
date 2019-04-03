ae8fad3ad62c5d7553ec29a1c93eb61806823147
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-04-03 12:07:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,9 @@
 class Dog 
 
-  def initialize(name)
-    @name = name 
-  end 
-
-  def initialize(breed= "Mutt")
+  def initialize(name, breed = "Mutt")
+    @name = name
     @breed = breed
-  end
+  end 
 
 
 end 
